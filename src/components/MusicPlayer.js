790db9d6208442c8f63309e5c7eca8064b05481a
Queenofc <152055731+Queenofc@users.noreply.github.com>
2025-03-02 17:50:29 +0530

    music player configured

--- src/components/MusicPlayer.js
+++ src/components/MusicPlayer.js
@@ -10,7 +10,7 @@ import { toast, ToastContainer } from "react-toastify";
 const moodData = {
   work: {
     title: "Simple Harmony",
-    url: "/audio/meditation.mp3",
+    url: "/audio/Idea10.mp3",
     color: "#40E0D0",
     image: work,
   },
